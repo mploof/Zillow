d5ab3fdf989b61a9aaf2db05c737f5cc3a40396f
mploof <michael.ploof@gmail.com>
2015-09-09 23:32:13 -0400

    Cleaned comments

--- src/zillow/XML.java
+++ src/zillow/XML.java
@@ -72,9 +72,7 @@ public class XML {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(fXmlFile);
-					
-			//optional, but recommended
-			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+
 			doc.getDocumentElement().normalize();
 						
 			Node nNode;
